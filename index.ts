d6e315503b7a33b9f0f107537e822e36adba9bc8
Bachal78 <160064284+Bachal78@users.noreply.github.com>
2024-03-24 22:05:33 +0500

    added console at the end

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-#! /usr/bin/env/node //
+#! /usr/bin/env/node
 
 import inquirer from "inquirer";
 
@@ -26,3 +26,4 @@ else if (answer.operator === "Division"){
 } else{
     console.log("Please select valid operator")
 }
+console.log("The End")
